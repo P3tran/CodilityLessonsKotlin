8c3f20dc583a24d0aeaa8954b1aa59568d200e72
petros <p3trann@gmail.com>
2019-10-07 20:41:55 +0300

    100% in lesson1, BinaryGap

--- src/lesson1/BinaryGap.kt
+++ src/lesson1/BinaryGap.kt
@@ -5,8 +5,31 @@ class BinaryGap {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-
+            val result = solution(1000000001)
+            println("result :  $result")
         }
     }
 }
 
+fun solution(N: Int): Int {
+    val binary = Integer.toBinaryString(N)
+    println(binary)
+    var maxGap = 0
+    if (binary.length < 3)
+        return maxGap
+
+    var isCounting = false
+    var runningGap = 0
+
+    for (i in binary.indices) {
+        if (binary[i] == '1') {
+            if (isCounting) {
+                maxGap = Math.max(runningGap, maxGap)
+                runningGap = 0
+            } else
+                isCounting = true
+        } else
+            runningGap++
+    }
+    return maxGap
+}
