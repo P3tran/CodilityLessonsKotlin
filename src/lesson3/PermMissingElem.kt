66bbf8652dc23703cd9e1ab12b316fb0a7a668fd
petros <p3trann@gmail.com>
2019-10-08 18:55:30 +0300

    100% in lesso 3 perm missing elem

--- src/lesson3/PermMissingElem.kt
+++ src/lesson3/PermMissingElem.kt
@@ -1,2 +1,28 @@
 package lesson3
 
+class PermMissingElem {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val result = solution(intArrayOf(1, 2, 3, 5, 4, 7, 8, 6))
+            println("result :  $result")
+
+        }
+
+        fun solution(A: IntArray): Int {
+            val set = hashSetOf<Int>()
+            if(A.isEmpty())
+                return 1
+
+            A.forEach {
+                set.add(it)
+            }
+            for (i in 1 .. A.size+1) {
+                if(!set.contains(i))
+                    return i
+            }
+            return 1
+        }
+    }
+}
