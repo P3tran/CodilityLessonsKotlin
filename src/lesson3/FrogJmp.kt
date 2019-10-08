c14277c3cb3688a47bdeb741e422300ca8f358b9
petros <p3trann@gmail.com>
2019-10-08 18:45:33 +0300

    100% in lesson 3 frog jump

---
+++ src/lesson3/FrogJmp.kt
@@ -0,0 +1,24 @@
+package lesson3
+
+class FrogJmp {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val result = solution(10, 150, 10)
+            println("result :  $result")
+
+        }
+
+        fun solution(X: Int, Y: Int, D: Int): Int {
+            val distanceToCover = Y - X
+            if (distanceToCover == 0)
+                return 0
+
+            if(distanceToCover.rem(D) == 0)
+                return distanceToCover/D
+            else
+                return distanceToCover/D +1
+        }
+    }
+}
