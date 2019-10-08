9e13349620448ce2d293ee94b0ea35a613db9dee
petros <p3trann@gmail.com>
2019-10-08 19:59:39 +0300

    100% lesson 3 tape equilibrium

---
+++ src/lesson3/TapeEquilibrium.kt
@@ -0,0 +1,30 @@
+package lesson3
+
+import kotlin.math.abs
+
+class TapeEquilibrium {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val result = solution(intArrayOf(1, 2, 3, 5, 4, 7, 8, 6))
+            println("result :  $result")
+
+        }
+
+        fun solution(A: IntArray): Int {
+            val total = A.sum()
+            var runningSum = 0
+            var minDifference = Integer.MAX_VALUE
+            var currentDifference: Int
+
+            for (i in 0 until A.size - 1) {
+                runningSum += A[i]
+                currentDifference = abs(runningSum - (total - runningSum))
+                minDifference = Math.min(currentDifference, minDifference)
+            }
+
+            return minDifference
+        }
+    }
+}
