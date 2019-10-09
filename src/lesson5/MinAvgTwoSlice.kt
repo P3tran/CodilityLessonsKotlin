e230dc2a98e8faa36d2112a527c79754c1146214
petros <p3trann@gmail.com>
2019-10-09 22:36:26 +0300

    60% in lesson 5 min avg two slice

---
+++ src/lesson5/MinAvgTwoSlice.kt
@@ -0,0 +1,38 @@
+package lesson5
+
+import kotlin.math.min
+
+/*
+* Find the minimum slice sum average in an array
+* */
+
+class MinAvgTwoSlice {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val result = solution(intArrayOf(-4, 4, 4, 7, -3, -4, 3, 2, 5, 6, 5))
+            println("result :  $result")
+        }
+
+        fun solution(A: IntArray): Int {
+            var min = Double.MAX_VALUE
+            var minIndex = 0
+
+            for (i in A.indices) {
+                var runningSum = A[i]
+                for (j in i+1 until A.size) {
+                    runningSum += A[j]
+                    val sumAverage = runningSum.toDouble() / (j - i + 1)
+                    if(sumAverage < min) {
+                        min = min(sumAverage, min)
+                        minIndex = i
+                    }
+                }
+            }
+
+            println(min)
+            return minIndex
+        }
+    }
+}
